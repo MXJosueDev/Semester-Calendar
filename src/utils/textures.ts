d3af243e79d60951087974f6e2905bfbec51c09f
Josue Lopez <69768362+MXJosueDev@users.noreply.github.com>
2026-07-18 14:48:22 -0600

    update version to 0.2.2 and adjust class names for overflow handling

--- src/utils/textures.ts
+++ src/utils/textures.ts
@@ -1,3 +1,3 @@
 export const getTextureURL = (texture: string) => {
-  return `${import.meta.env.BASE_URL}/assets/textures/${texture}`;
+  return `${import.meta.env.BASE_URL}assets/textures/${texture}`;
 };
