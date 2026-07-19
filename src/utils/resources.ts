f4c973fc73cd734e4c29eda45756065bd3ad1e0d
Josue Lopez <69768362+MXJosueDev@users.noreply.github.com>
2026-07-18 22:13:02 -0600

    refactor: use joinRoutes function for URL construction in resource functions

--- src/utils/resources.ts
+++ src/utils/resources.ts
@@ -1,11 +1,15 @@
+export const joinRoutes = (...routes: string[]): string => {
+  return routes.map(route => route.replace(/(^\/+|\/+$)/g, '')).join('/');
+}
+
 export const getTextureURL = (texture: string) => {
-  return `${import.meta.env.BASE_URL}assets/textures/${texture}`;
+  return joinRoutes(`${import.meta.env.BASE_URL}`, `/assets/textures/${texture}`);
 };
 
 export const getMusicURL = (music: string) => {
-  return `${import.meta.env.BASE_URL}assets/music/${music}`;
+  return joinRoutes(`${import.meta.env.BASE_URL}`, `/assets/music/${music}`);
 }
 
 export const getSoundURL = (sound: string) => {
-  return `${import.meta.env.BASE_URL}assets/sounds/${sound}`;
+  return joinRoutes(`${import.meta.env.BASE_URL}`, `assets/sounds/${sound}`);
 }
