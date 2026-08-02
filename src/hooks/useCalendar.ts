60ecf41fc14e54c6286b9488f0e4ea2165090000
Josue Lopez <69768362+MXJosueDev@users.noreply.github.com>
2026-08-02 14:22:20 -0600

    feat: implement page visibility hook and integrate into components for improved performance

--- src/hooks/useCalendar.ts
+++ src/hooks/useCalendar.ts
@@ -1,23 +1,28 @@
 import { useState, useEffect } from "react";
-// Importa tus funciones desde el archivo donde las definiste
 import { getElapsedWeeks } from "@/utils/calendar.ts";
+import usePageVisibility from "./usePageVisibility";
 
 const useCalendar = () => {
   const [elapsedWeeks, setElapsedWeeks] = useState<number>(getElapsedWeeks());
+  const isPageVisible = usePageVisibility();
 
   useEffect(() => {
+    if (!isPageVisible) {
+      return;
+    }
+
     const checkTimeProgress = () => {
       const currentElapsed = getElapsedWeeks();
 
-      if (currentElapsed !== elapsedWeeks) {
-        setElapsedWeeks(currentElapsed);
-      }
+      setElapsedWeeks(previousElapsed =>
+        previousElapsed === currentElapsed ? previousElapsed : currentElapsed
+      );
     };
 
     const interval = setInterval(checkTimeProgress, 1000 * 60 * 1);
 
     return () => clearInterval(interval);
-  }, [elapsedWeeks]);
+  }, [isPageVisible]);
 
   return elapsedWeeks;
 };
